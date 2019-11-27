505b1554cb90e8b84e2e1e8d537565cffae21d17
Taras Pohoretskyi <ci4anish@gmail.com>
2019-11-27 19:05:02 +0200

    start filter method

--- src/components/FlatList/FlatList.jsx
+++ src/components/FlatList/FlatList.jsx
@@ -1,35 +1,55 @@
 import React from "react";
+import {Select} from 'react-select';
 
-import { FlatItem } from '../index';
+import {FlatItem} from '../index';
 
 class FlatList extends React.Component {
   state = {
     data: [],
+    options: [],
   };
+
+  // const options = [
+  //   { value: 'chocolate', label: 'Chocolate' },
+  //   { value: 'strawberry', label: 'Strawberry' },
+  //   { value: 'vanilla', label: 'Vanilla' }
+  // ]
 
   componentDidMount() {
     this.getData();
+  }
+
+  getOptions(data) {
+    return data.map(item => {
+      return {value: item.layoutType, label: item.layoutType};
+    });
+  }
+
+  filterData() {
+
   }
 
   getData() {
     fetch(`http://localhost:8000/data`)
       .then(response => response.json())
       .then(data => {
-        this.setState({ data });
+        this.setState({data: data, options: this.getOptions(data)});
       });
   }
 
   render() {
-    const { data } = this.state;
+    const {data,options} = this.state;
 
     return (
+      {/*<Select options={options} />*/}
+
       <div className="flat-list">
         {
-          data.map(item => <FlatItem data={item} />)
+          data.map(item => <FlatItem data={item}/>)
         }
       </div>
     )
   }
 }
 
-export { FlatList };
+export {FlatList};
