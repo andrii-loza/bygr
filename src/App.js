c5f0ee2b77ba72b29203eb65e14b74eea7df9f0c
Taras Pohoretskyi <ci4anish@gmail.com>
2019-11-27 17:18:18 +0200

    add get methods

--- src/App.js
+++ src/App.js
@@ -1,14 +1,47 @@
 import React from 'react';
 import './App.css';
 
-function App() {
-    return (
-        <div className="App">
-            <center>
-                <h1>Hello</h1>
-            </center>
-        </div>
-    );
+class App extends React.Component {
+
+    componentDidMount() {
+        this.getData();
+    }
+
+    getData() {
+        fetch(`http://localhost:8000/data`)
+            .then(responce => responce.json())
+            .then(data => {
+                console.log(data);
+                debugger;
+            });
+    }
+
+    getDataByFloor(floor) {
+        fetch(`http://localhost:8000/buyer-info/${floor}`)
+            .then(response => response.json())
+            .then(data => {
+                console.log(data);
+                debugger;
+                this.setState({data});
+            });
+    }
+
+    render() {
+        return (
+            <div className="App">
+                <center>
+                    <h1>Hello</h1>
+                </center>
+
+                <button onClick={() => this.getDataByFloor(1)}>Floor 1</button>
+                <button onClick={() => this.getDataByFloor(2)}>Floor 2</button>
+                <button onClick={() => this.getDataByFloor(3)}>Floor 3</button>
+                <button onClick={() => this.getDataByFloor(4)}>Floor 4</button>
+            </div>
+        );
+    }
+
+
 }
 
 export default App;
