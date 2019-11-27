f4ae696681154364997062f70fb1f83e8203c57b
Taras Pohoretskyi <ci4anish@gmail.com>
2019-11-27 16:09:34 +0200

    first commit

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+    return (
+        <div className="App">
+            <center>
+                <h1>Hello</h1>
+            </center>
+        </div>
+    );
+}
+
+export default App;
